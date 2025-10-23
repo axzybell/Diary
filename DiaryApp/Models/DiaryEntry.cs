576041f3bfa96548a1c743bdc2a1a879331ae5bc
Chong Jun Wei <chongjw001160@gmail.com>
2025-10-23 14:00:11 +0800

    Add project files.

---
+++ DiaryApp/Models/DiaryEntry.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DiaryApp.Models
+{
+    public class DiaryEntry
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string? Title { get; set; }
+
+        [Required]
+        public string? Content { get; set; }
+
+        [Required]
+        public DateTime DateCreated { get; set; } = DateTime.Now;
+    }
+}

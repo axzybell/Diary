7262004f3e180c6d28ea59c7366135ced1bed7f4
Chong Jun Wei <chongjw001160@gmail.com>
2025-10-23 15:07:36 +0800

    Seeding data values in DiaryEntries - add-migration AddedSeedingDataDiaryEntry - update-database

--- DiaryApp/Data/ApplicationDbContext.cs
+++ DiaryApp/Data/ApplicationDbContext.cs
@@ -11,5 +11,34 @@ namespace DiaryApp.Data
         }
 
         public DbSet<DiaryEntry> DiaryEntries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<DiaryEntry>().HasData(
+                new DiaryEntry 
+                { 
+                    Id = 1, 
+                    Title="Went Hiking", 
+                    Content="When Hiking with Joe!", 
+                    DateCreated= new DateTime(2025, 10, 23, 14, 49, 50)
+                },
+                new DiaryEntry
+                {
+                    Id = 2,
+                    Title = "Went Shopping",
+                    Content = "When Shopping with Joe!",
+                    DateCreated = new DateTime(2025, 10, 23, 14, 49, 50)
+                },
+                new DiaryEntry
+                {
+                    Id = 3,
+                    Title = "Went Diving",
+                    Content = "When Diving with Joe!",
+                    DateCreated = new DateTime(2025, 10, 23, 14, 49, 50)
+                }
+             );
         }
+    }
 }
---
+++ DiaryApp/Migrations/20251023070431_AddedSeedingDataDiaryEntry.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace DiaryApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedSeedingDataDiaryEntry : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "DiaryEntries",
+                columns: new[] { "Id", "Content", "DateCreated", "Title" },
+                values: new object[,]
+                {
+                    { 1, "When Hiking with Joe!", new DateTime(2025, 10, 23, 14, 49, 50, 0, DateTimeKind.Unspecified), "Went Hiking" },
+                    { 2, "When Shopping with Joe!", new DateTime(2025, 10, 23, 14, 49, 50, 0, DateTimeKind.Unspecified), "Went Shopping" },
+                    { 3, "When Diving with Joe!", new DateTime(2025, 10, 23, 14, 49, 50, 0, DateTimeKind.Unspecified), "Went Diving" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "DiaryEntries",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "DiaryEntries",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "DiaryEntries",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}

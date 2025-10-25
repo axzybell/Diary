18e9e4d026544bbffa1a938ae15ec8b77d33dec2
Chong Jun Wei <chongjw001160@gmail.com>
2025-10-25 14:16:05 +0800

    Edit page route and display the selected Diary's info

--- DiaryApp/Controllers/DiaryEntriesController.cs
+++ DiaryApp/Controllers/DiaryEntriesController.cs
@@ -51,5 +51,21 @@ namespace DiaryApp.Controllers
 
             return View(obj);
         }
+
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0) { 
+                return NotFound();
+            }
+
+            DiaryEntry? diaryEntry = _db.DiaryEntries.Find(id);
+
+            if (diaryEntry == null) {
+                return NotFound();
+            }
+
+            return View(diaryEntry);
+        }
     }
 }
